e581fd5ac056b860e9b4deb0c242f4f813249825
Chaimaa5 <chaimaa.elmhandez@gmail.com>
2023-03-13 21:15:25 +0100

    Sockets finished

--- Socket/Manager.cpp
+++ Socket/Manager.cpp
@@ -32,45 +32,45 @@ void Manager::Start(std::vector<Server> servers){
 			sock.SockCreate(servers[i]);
 			socket.push_back(sock);
 			fd.push_back(sock.GetSockFd());
-			FD_SET(sock.GetSockFd(), &_readfds) ;
 		}
 		ports.push_back(port);
 	}
+	
 	Accepter();
 }
 
-void Manager::Accepter(){
+void Manager::Accepter() {
+    int new_socket;
+    int res;
+    while (1) {
+        FD_ZERO(&_readfds);
+        FD_ZERO(&_writefds);
+        for (size_t i = 0; i < socket.size(); i++)
+            FD_SET(fd[i], &_readfds);
+        _tmpreadfds = _readfds;
+        _tmpwritefds = _writefds;
+        if ((res = select(fd.back() + 1, &_tmpreadfds, NULL, NULL, NULL)) < 0)
+            std::cout << "select failed\n";
+        for (size_t i = 0; i < socket.size(); i++) {
+            if (FD_ISSET(fd[i], &_tmpreadfds)) {
+                printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+                if ((new_socket = accept(fd[i], (struct sockaddr *)&socket[i].serv_addr, (socklen_t*)&socket[i].addrlen))<0) {
+                    perror("In accept");
+                    exit(EXIT_FAILURE);
+                }
+                
+                char buffer[300000] = {0};
 
-	int new_socket;
-	int res;
-	while(1)
-    {
-		int sockfd = fd[fd.size() - 1];
-		
-		_tmpreadfds = _readfds;
-		_tmpwritefds = _writefds;
-		if ((res = select(sockfd + 1, &_tmpreadfds, NULL, NULL, 0)) < 0)
-			std::cout << "select failed\n";
-		if (FD_ISSET(sockfd, &_tmpreadfds)){
-        	printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-			for (size_t i = 0; i < socket.size(); i++)
-			{
-				if ((new_socket = accept(sockfd, (struct sockaddr *)&socket[i].serv_addr, (socklen_t*)&socket[i].addrlen))<0)
-				{
-					std::cout << sockfd << std::endl;
-					perror("In accept");
-					exit(EXIT_FAILURE);
-				}
-				FD_SET(new_socket, &_tmpwritefds);
-				FD_CLR(sockfd, &_tmpreadfds);
-				char buffer[300000] = {0};
-				recv( new_socket , buffer, 300000, MSG_DONTWAIT);
-				printf("%s\n",buffer );
-				close(new_socket);
-			}
-		}
-		if (FD_ISSET(new_socket, &_tmpwritefds))
-			write(new_socket, "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 20\n\nHello!", 68);
+                recv(new_socket, buffer, 300000, MSG_DONTWAIT);
+                printf("%s\n", buffer);
+                
+
+                FD_SET(new_socket, &_tmpreadfds);
+                close(new_socket);
+            }
+        }
+        // if (FD_ISSET(new_socket, &_tmpwritefds))
+        // 	write(new_socket, "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 20\n\nHello!", 68);
     }
 }
 
